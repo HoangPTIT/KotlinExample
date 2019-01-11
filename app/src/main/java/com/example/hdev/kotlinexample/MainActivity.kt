589b9a2be14bbd1f6083a3618fa7d46c766973cf
HoangIT <buihoangbg01@gmail.com>
2019-01-11 17:21:49 +0700

    Vong lap For

--- app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
@@ -9,12 +9,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // TODO: Vong lap While
-        var a = 0;
-        while (a < 10) {
-            Log.d(TAG, "Gia tri: $a")
-            a++;
+        // TODO: Vong lap For
+        // i chay tu 0 -> 3
+        for (i in 0..3) {
+            Log.d(TAG, "Gia tri i: $i")
+        }
+        // VD: Chay tu 0 den bien a.
+        var a = 3
+        // i chay tu 0 -> 2. until-1
+        for (i in 0 until a) {
+            Log.d(TAG, "Until Gia tri i: $i")
+        }
+        // Lap nguoc: a -> 0
+        for (i in a downTo 0) {
+            Log.d(TAG, "Lap nguoc: Gia tri i: $i")
+        }
+        // Moi lan lap +b
+        for (i in 0..a step 2){
+            Log.d(TAG, "Step 2: Gia tri i: $i")
         }
     }
 }
