3dd96b83c8562f2c78fbbb16ceafbfad542a531c
HoangIT <buihoangbg01@gmail.com>
2019-01-11 17:02:26 +0700

    Vong lap ÆÆWhile

--- app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
@@ -10,28 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO: Cau lenh When: Bat nhieu dieu kien - switch:case
-        var month = 4;
-        when (month) {
-            // in 1..3: Trong khoang
-            1, 2, 3 -> {
-                Log.d(TAG, "Thang: $month")
-                Log.d(TAG, "Quy I")
-            }
-            4, 5, 6 -> {
-                Log.d(TAG, "Thang: $month")
-                Log.d(TAG, "Quy II")
-            }
-            7, 8, 9 -> {
-                Log.d(TAG, "Thang: $month")
-                Log.d(TAG, "Quy III")
-            }
-            10, 11, 12 -> {
-                Log.d(TAG, "Thang: $month")
-                Log.d(TAG, "Quy IV")
-            }
-            else -> Log.d(TAG, "Khong co gia tri quy")
+        // TODO: Vong lap While
+        var a = 0;
+        while (a < 10) {
+            Log.d(TAG, "Gia tri: $a")
+            a++;
         }
-
     }
 }
