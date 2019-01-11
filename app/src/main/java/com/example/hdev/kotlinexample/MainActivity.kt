e8f3f1ec405a8b29f81603dc7c89b6efbba913b8
HoangIT <buihoangbg01@gmail.com>
2019-01-11 16:03:44 +0700

    If-else trong Kotlin (TT Java)

--- app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
@@ -9,14 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var canBeNull: String? = "Something" // Bien nay co the null. Them "?" sau kieu du lieu
-        canBeNull = canBeNull?.toLowerCase(); // Co the check null truoc
-        var cantBeNull: String = "hoang"!! // Khong duoc phep null
-        /*Khi goi bien co the null ma chua check null truoc, phai sd ? hoac !! de qd co chay lenh nay hay khong?
-        * ?: null se bo qua lenh, nguoc lai se chay. Co the gan ve null
-        * !!: null: Bao loi nhu java. Khong duoc gan ve null.
-        * */
-        // Check null trong Kotlin. ?: se check null. Null -> Gan gia tri mac dinh va nguoc lai.
-        var someThing = canBeNull ?: "default"
+
+        // TODO: If - else
+        var a = 10
+        var b = 15
+        if (a > b){
+            Log.d(TAG, "A lon hon B")
+        }else{
+            Log.d(TAG, "A nho hon B")
+        }
     }
 }
