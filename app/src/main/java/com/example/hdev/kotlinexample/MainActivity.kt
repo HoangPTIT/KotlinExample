43e16cc1964eab7fcc74f3e114802611f7cfc8eb
HoangIT <buihoangbg01@gmail.com>
2019-01-11 15:50:21 +0700

    Null Safety ? or git add .

--- app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
@@ -5,20 +5,18 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    val TAG : String = "MainActivity"
+    val TAG: String = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Khai bao kieu du lieu String
-        var sName = "Hoang"
-        var sAge = 2_2 // Co the su dung gach chan giua cac so ma gia tri khong thay doi (22)
-        // Trong String co the truyen truc tiep bien vao ma khong phai cong chuoi nhu java.
-        var price = 5;
-        var total_price = "Price: $price"
-        Log.d(TAG, "" + price)
-        Log.d(TAG, "" + total_price)
-        Log.d(TAG, "" + sName)
-        Log.d(TAG, "" + sAge)
+        var canBeNull: String? = "Something" // Bien nay co the null. Them "?" sau kieu du lieu
+        canBeNull = canBeNull?.toLowerCase(); // Co the check null truoc
+        var cantBeNull: String = "hoang"!! // Khong duoc phep null
+        /*Khi goi bien co the null ma chua check null truoc, phai sd ? hoac !! de qd co chay lenh nay hay khong?
+        * ?: null se bo qua lenh, nguoc lai se chay. Co the gan ve null
+        * !!: null: Bao loi nhu java. Khong duoc gan ve null.
+        * */
+        // Check null trong Kotlin. ?: se check null. Null -> Gan gia tri mac dinh va nguoc lai.
+        var someThing = canBeNull ?: "default"
     }
 }
