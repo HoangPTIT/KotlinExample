cef3fd192361a48e6e68f17f93b445b29c533819
HoangIT <buihoangbg01@gmail.com>
2019-01-12 09:36:15 +0700

    Mang trong Kotlin

--- app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
@@ -9,24 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // TODO: Vong lap For
-        // i chay tu 0 -> 3
-        for (i in 0..3) {
-            Log.d(TAG, "Gia tri i: $i")
-        }
-        // VD: Chay tu 0 den bien a.
-        var a = 3
-        // i chay tu 0 -> 2. until-1
-        for (i in 0 until a) {
-            Log.d(TAG, "Until Gia tri i: $i")
-        }
-        // Lap nguoc: a -> 0
-        for (i in a downTo 0) {
-            Log.d(TAG, "Lap nguoc: Gia tri i: $i")
-        }
-        // Moi lan lap +b
-        for (i in 0..a step 2){
-            Log.d(TAG, "Step 2: Gia tri i: $i")
-        }
+        // TODO: Mang: 3 cach khai bao
+        // Cach 1: Khong co mang String (StringArray)
+        var numArray: IntArray = intArrayOf(1, 2, 3, 4); // int[] a = new int(4)
+        // Cach 2: Co mang String
+        var numArray2: List<String> = listOf("A", "B", "C"); // List<String> a = new List<>()
+        // Cach 3: Khoi tao ArrayList roi add element.(Thuong dung)
+        var array3: ArrayList<String> = ArrayList(); // ArrayList<String> b = new ArrayList<>()
+        array3.add("A")
+        array3.add("B")
     }
 }
