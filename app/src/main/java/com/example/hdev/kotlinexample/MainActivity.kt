55759683f07a99957c2b6bf0cfec1ff96d736a5e
HoangIT <buihoangbg01@gmail.com>
2019-01-12 15:52:43 +0700

    inner-Asynctask doc noi dung URL Internet

--- app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/example/hdev/kotlinexample/MainActivity.kt
@@ -1,18 +1,52 @@
 package com.example.hdev.kotlinexample
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     val TAG: String = "MainActivity"
+    val URL_LINK = "http://khoapham.vn/"
     // La ham override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // TODO: Data Class. La 1 class da chua du lieu.
+        // TODO: Su dung Asynctask doc noi dung URL internet
+        ReadContentURL().execute(URL_LINK)
+    }
 
-        val transport : Transport = Transport("Bus", "Red")
-        Log.d(TAG, "Transport: ${transport.name} ${transport.color}")
+    inner class ReadContentURL : AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg params: String?): String {
+            var content: StringBuilder = StringBuilder()
+            val url: URL = URL(params[0])
+            val urlCon: HttpURLConnection = url.openConnection() as HttpURLConnection
+            val inputStream: InputStream = urlCon.inputStream
+            val inputStreamReader: InputStreamReader = InputStreamReader(inputStream)
+            val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+            var line : String = ""
+            try {
+                do {
+                    line = bufferedReader.readLine()
+                    if (line != null){
+                        content.append(line)
+                    }
+                } while (line != null)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return content.toString()
+        }
+
+        override fun onPostExecute(result: String?) {
+            txt_content.setText(result)
+        }
     }
 }
